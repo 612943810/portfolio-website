6f0fd2a05b4ae4e33863b3301dc5f0000f51798b
Raiyan Rofiquzzaman <raiyanrofiquzzaman@hotmail.com>
2022-03-20 03:13:51 -0400

    This update includes updates to the presentation of the portfolio.

--- specialEffects.js
+++
@@ -1,5 +0,0 @@
-tippy('#link',{
-    content:'<h2>This Emploment management System Backend uses Express,MongoDB to do Crud operations.<h2><br> <a href="www.google.com">Hello</a>',
-    allowHTML:true,
-    trigger:'click'
-})
